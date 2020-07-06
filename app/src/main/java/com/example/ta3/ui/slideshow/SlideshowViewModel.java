340b83d2181eb6b654a8de3c5981f9fce8e03a29
Lucky Ahmad Ardiansyah <lucky.ahmad.ft18@mail.umy.ac.id>
2020-07-06 23:21:37 +0700

    comit

---
+++ app/src/main/java/com/example/ta3/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.ta3.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Related Video");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
