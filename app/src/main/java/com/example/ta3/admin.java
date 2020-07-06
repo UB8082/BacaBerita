c30f9e032bbaf0e0201dc6f9ee991dc7d67911ef
Lucky Ahmad Ardiansyah <lucky.ahmad.ft18@mail.umy.ac.id>
2020-07-06 23:37:41 +0700

    comit

---
+++ app/src/main/java/com/example/ta3/admin.java
@@ -0,0 +1,14 @@
+package com.example.ta3;
+
+public class admin {
+
+    public String username;
+    public String email;
+
+
+    public admin(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
